eed4f80807b971a64498c290bdb367ec4ed51acf
89653126109 <86475178+89653126109@users.noreply.github.com>
2021-08-04 08:15:29 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,36 @@
-const gulp = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const gulpStylelint = require('gulp-stylelint');
 // const critical = require('critical').stream;
 const cleanCSS = require('gulp-clean-css');
 
-function style() {
-    return gulp.src('./css/**/*.scss')
-            .pipe(sass())
-            .pipe(cleanCSS())
-            .pipe(gulp.dest('./css'))
-            .pipe(browserSync.stream())
+function style () {
+    return src('./css/**/*.scss')
+        .pipe(gulpStylelint({
+            reporters: [
+                {
+                    formatter: 'string',
+                    console: true
+                }
+            ]
+        }))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
+        .pipe(dest('./css/'))
+        .pipe(browserSync.stream());
 }
 
-function watch () {
+function watcher () {
     browserSync.init({
         server: {
-          baseDir: './'
-        }  
+            baseDir: './'
+        }
     });
-    gulp.watch('./css/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+    watch('./css/**/*.scss', style);
+    watch('./*html').on('change', browserSync.reload);
+    watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
-// function taskCritical () {
-//     return src('./*.html')
-//         .pipe(
-//             critical({
-//                 base: './',
-//                 inline: true,
-//                 css: ['css/style.css'],
-//             })
-//         )
-//         .on('error', err => {
-//             log.error(err.message);
-//         })
-//         .pipe(dest('./'))
-// }
-
-// exports.critical = taskCritical;
 exports.style = style;
-exports.watch = watch;
+exports.watch = watcher;
